5da0e020caa20e2db0feafdbe320af8552a780b4
Fábio Rosa <fabiomrosa@gmail.com>
2018-03-16 00:04:27 -0300

    Init store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
+import configureStore from './store/configureStore';
 import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/style.css';
 
 const history = createBrowserHistory();
+const store = configureStore();
 
 ReactDOM.render(
-  <Router history={ history }>
-    { routes }
-  </Router>,
+  <Provider store={ store }>
+    <Router history={ history }>
+      { routes }
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
